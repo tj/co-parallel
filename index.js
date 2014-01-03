619d2d1d78d576ea3aea0fc5313bee0262e64bfe
Yazhong Liu <yorkiefixer@gmail.com>
2014-01-03 18:34:55 +0800

    refactor for #1

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-
 module.exports = function *parallel(thunks, n){
+  var n = n || 5;
   var ret = [];
-  n = n || 5;
+  var first = thunks.slice(0, n);
+  var queue = thunks.slice(n);
 
-  while (thunks.length) {
-    var res = yield thunks.slice(0, n);
-    ret = ret.concat(res);
-    thunks = thunks.slice(n);
-  }
+  var res = yield first.map(function *(item) {
+    var curr = yield item;
+    var next = queue.pop();
+    if (next)
+      ret.push(yield next);
 
+    return curr;
+  })
+  ret = ret.concat(res);
+
+  while (queue.length);
   return ret;
 };
